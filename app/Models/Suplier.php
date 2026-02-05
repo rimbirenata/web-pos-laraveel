23998eeee37395f96675fd2ee62775d57e6e6af8
Rimbi Renata <rimbi.renata5@smk.belajar.id>
2026-02-05 07:36:30 +0700

    baru99

--- app/Models/Suplier.php
+++ app/Models/Suplier.php
@@ -8,11 +8,17 @@ class Suplier extends Model
 {
     protected $table = 'suplier';
     protected $primaryKey = 'id_suplier';
-    public $timestamps = false; 
+    public $timestamps = false;
 
     protected $fillable = [
         'nama_suplier',
         'alamat',
         'no_hp'
     ];
+
+    // 🔗 RELASI: 1 supplier punya banyak barang
+    public function barang()
+    {
+        return $this->hasMany(Barang::class, 'suplier_id', 'id_suplier');
+    }
 }
